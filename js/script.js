0f3259b067cd88dd8c3acd7e451fb739b681301e
hironmoy-ray <hpray35@gmail.com>
2024-03-06 00:53:50 +0600

    Static Varification Login Form

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById("btn-submit").addEventListener("click", function () {
+    const emailField = document.getElementById("email-field");
+    const email = emailField.value;
+    // console.log(email);
+    const passwordField = document.getElementById("password-field");
+    const password = passwordField.value;
+    // console.log(password);
+    if (email === "bankofdady@gmail.com" && password === "1234") {
+      window.location.href = "insideOfBank.html";
+    } else {
+      if (email !== "bankofdady@gmail.com") {
+        alert("Please Enter Valid Email");
+      } else {
+        alert("Please Enter Correct Password");
+      }
+    }
+  });
+  
